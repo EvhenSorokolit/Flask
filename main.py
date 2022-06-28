93885fac3d9e727d85967eba34c022e8caa83246
Evhen Sorokolit <Sonykillstation@gmail.com>
2022-06-28 15:58:58 +0300

    forms

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 menu = [{"name": "Установка","url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
-        {"name": "Обратная связь", "url": "content"}]
+        {"name": "Обратная связь", "url": "contact"}]
+
+
 @app.route("/index")
 @app.route("/")
 def index():
@@ -14,7 +16,15 @@ def index():
 @app.route("/about")
 def about():
     print(url_for('about'))
-    return render_template('about.html', title="О сайте",menu=menu)
+    return render_template('about.html', title="О сайте", menu=menu)
+
+
+@app.route("/contact",methods=["POST","GET"])
+def contact():
+    if request.method =='POST':
+        print(request.form['username'])
+    return render_template('contact.html', title="Обратная связь", menu=menu)
+
 
 @app.route("/profile/<path:username>")
 def profile(username):
