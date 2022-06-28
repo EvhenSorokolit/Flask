ee38fa2862f036c8092a21d5c82180092f8f53f8
Evhen Sorokolit <Sonykillstation@gmail.com>
2022-06-28 15:42:27 +0300

    static resources css js etc

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
-menu = ["Установка","Первое приложение","Обратная связь"]
+menu = [{"name": "Установка","url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "content"}]
 @app.route("/index")
 @app.route("/")
 def index():
@@ -19,10 +21,10 @@ def profile(username):
     return f"Пользователь: {username}"
 
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('about'))
-    print(url_for('profile', username="test"))
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username="test"))
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
