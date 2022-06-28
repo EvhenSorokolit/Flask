b5927dc53fa27692b1a08d8e83b4a1b9a1e81d08
Evhen Sorokolit <Sonykillstation@gmail.com>
2022-06-28 17:37:09 +0300

    подключаем ДБ

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fdsfsdfsdfsdf'
 menu = [{"name": "Установка", "url": "install-flask"},
