9a059802789d500fd609cd0555b3255f50407e6f
Evhen Sorokolit <Sonykillstation@gmail.com>
2022-06-30 19:31:09 +0300

    registracia userov

--- flsite.py
+++ flsite.py
@@ -1,7 +1,10 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request, g, flash,abort
+from flask import Flask, render_template, request, g, flash, abort, redirect, url_for
 from FDataBase import FDataBase
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
 # config
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -12,11 +15,14 @@ app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 dbase = None
+
+
 @app.before_request
 def before_request():
     global dbase
     db = get_db()
     dbase = FDataBase(db)
+
 
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
@@ -43,27 +49,26 @@ def get_db():
 @app.route("/")
 @app.route("/index")
 def index():
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
-    return render_template('index.html', menu=dbase.getMenu(),posts= dbase.getPostsAnonce())
 
 @app.route("/post/<alias>")
 def showPost(alias):
-
-    title,post = dbase.getPost(alias)
+    title, post = dbase.getPost(alias)
     if not title:
         abort(404)
-    return render_template('post.html', menu=dbase.getMenu(),title=title, post=post)
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
 
 @app.route("/add_post", methods=["POST", "GET"])
 def addPost():
-
     if request.method == "POST":
         if len(request.form['name']) > 4 and len(request.form['post']) > 10:
             res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
             if not res:
                 flash('Ошибка добалвения статьи', category='error')
             else:
-                flash(' Статья успешно добавлена',category='success')
+                flash(' Статья успешно добавлена', category='success')
         else:
             flash('Ошибка добалвения статьи', category='error')
     return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
@@ -71,9 +76,35 @@ def addPost():
 
 @app.teardown_appcontext
 def close_db(error):
-    #close connection if it exist
+    # close connection if it exist
     if hasattr(g, 'link_db'):
         g.link_db.close()
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html', menu=dbase.getMenu(), title="Авторизация")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['email']) > 4 \
+            and len(request.form['psw']) > 4 and  len(request.form['psw']) == len(request.form['psw2']):
+            hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(request.form['name'],request.form['email'], hash)
+            if res:
+                flash("Вы успешно зарегистрированы", "success")
+                return  redirect(url_for('login'))
+            else:
+                flash(("Ошибка при добавлении в БД", "error"))
+        else:
+            flash(("Не верно заполнені поля", "error"))
+
+
+
+    return render_template('register.html', menu=dbase.getMenu(), title="Регистрация")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
